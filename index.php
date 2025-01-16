9ce4e9ff995e848675e88655731b1cb84cf98b68
240-p <fikriabdi151@gmail.com>
2025-01-15 19:07:35 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Portofolio</title>
+ <link rel="stylesheet" href="tampil.css">
+</head>
+<body>
+
+<header>
+
+ <nav>
+ <ul class="slide">
+ <li><a href="#tentang">Tentang</a></li>
+ <li><a href="#portofolio">Portofolio</a></li>
+ <li><a href="#kontak">Kontak</a></li>
+ </ul>
+ <div class="menu-toggle">
+  <input type="checkbox">
+  <span></span>
+  <span></span>
+  <span></span>
+  </div>
+ </nav>
+ </header>
+ 
+ <section id="tentang">
+ <div class="profil">
+    <img src="WhatsApp Image 2024-02-14 at 08.19.36.jpeg" alt="Fickry Abdi Rabbani" class="foto" >
+    <div class="info">
+      <h5>Fickry Abdi Rabbani</h5>
+      <h1>Desain Grafis</h1>
+      <p>Laki-laki</p>
+      <p>11-0*-****</p>
+      <p>Kuningan Jawa Barat</p>
+    </div>
+    
+    
+    <div class="info3" data-aos="fade-up" data-aos-duration="2000">
+        <h5>Dekrifsi Diri</h5>
+        <p>Lorem ipsum dolor sit amet, Vestibulum ac ligula at velit viverra fringilla a rutrum ante. Cras viverra est elit, ac fermentum augue convallis a. Suspendisse ac eros in eros commodo finibus. Sed mi metus, pulvinar quis sagittis eu, auctor eu nibh. Ut vulputate nec turpis quis dapibus. Sed maximus eget erat sit amet mollis. Nam imperdiet justo blandit, ultricies est at, luctus tellus. Mauris ut tortor quis turpis vulputate malesuada at vitae tellus. Donec euismod blandit sagittis. Nullam et tellus ipsum. Etiam rutrum nisi at sem posuere, a mattis magna auctor. Donec sodales molestie enim ac auctor.</p>
+      </div>
+ </div>
+ </section>
+
+ 
+ <div class="wave"></div>
+
+
+
+ <section id="portofolio">
+ <div class="info4" data-aos="fade-up" data-aos-duration="2000">
+        <h5>Portofolio Me</h5>
+        <p>Lorem ipsum dolor sit amet, a mattis magna auctor. Donec sodales molestie enim ac auctor.</p>
+      </div>
+ </div>
+
+
+
+
+
+ <!-- Art satu -->
+<section id="portofolio3" data-aos="flip-down"   data-aos-delay="50" data-aos-duration="2000"> 
+<button id="tombol3">Digital Art</button>
+<div id="galeri3" class="sembunyi3">
+    <div class="galeri3">
+    <img src="art/1.jpg" alt="Gambar 1" class="gambar3">
+   <img src="art/10.png" alt="Gambar 1" class="gambar3">
+   <img src="art/2.jpg" alt="Gambar 2" class="gambar3">                    
+   <img src="art/4.jpg" alt="Gambar 4" class="gambar3">
+   <img src="art/6.png" alt="Gambar 6" class="gambar3">
+   <img src="art/7.jpeg" alt="Gambar 7" class="gambar3">
+   <img src="art/8.jpeg" alt="Gambar 8" class="gambar3">
+   <img src="art/9.png" alt="Gambar 9" class="gambar3">
+   <img src="art/Tak berjudul90.png" alt="Gambar 9" class="gambar3">
+    </div>
+   </section>
+</div>
+   <div class="modal3" id="modal3"> 
+       <img src="" alt="" class="gambar-besar3">
+       <span class="tutup3">&times;</span>
+   </div>
+
+<!-- Adobe ilustator -->
+     <section id="portofolio" data-aos="flip-right"   data-aos-delay="50" data-aos-duration="2000"> 
+<button id="tombol2">Adobe Ilustator</button>
+<div id="galeri2" class="sembunyi2">
+    <div class="galeri2">
+    <img src="AI/3.jpg" alt="Gambar 3" class="gambar2">
+    <img src="AI/5.jpg" alt="Gambar 5" class="gambar2">
+    <img src="AI/pinguin.jpg" alt="Gambar 5" class="gambar2">
+    <img src="AI/koko.jpg" alt="Gambar 5" class="gambar2">
+    </div>
+   </section>
+</div>
+   <div class="modal2" id="modal2"> 
+       <img src="" alt="" class="gambar-besar2">
+       <span class="tutup2">&times;</span>
+   </div>
+
+
+ 
+
+   
+<section id="portofolio" data-aos="flip-right"   data-aos-delay="50" data-aos-duration="2000"> 
+<button id="tombol2">Adobe Ilustator</button>
+<div id="galeri2" class="sembunyi2">
+    <div class="galeri2">
+    <img src="AI/3.jpg" alt="Gambar 3" class="gambar2">
+    <img src="AI/5.jpg" alt="Gambar 5" class="gambar2">
+    <img src="art/10.png" alt="Gambar 1" class="gambar2"> 
+    <img src="AI/pinguin.jpg" alt="Gambar 5" class="gambar2">
+    <img src="AI/koko.jpg" alt="Gambar 5" class="gambar2">
+    </div>
+   </section>
+</div>
+   <div class="modal2" id="modal2"> 
+       <img src="" alt="" class="gambar-besar2">
+       <span class="tutup2">&times;</span>
+   </div>
+
+
+   <section id="portofolio" data-aos="flip-right"   data-aos-delay="50" data-aos-duration="2000"> 
+<button id="tombol2">Adobe XD</button>
+<div id="galeri2" class="sembunyi2">
+    <div class="galeri2">
+    </div>
+   </section>
+   
+   <div class="modal2" id="modal2"> 
+       <img src="" alt="" class="gambar-besar2">
+       <span class="tutup2">&times;</span>
+   </div>
+
+
+
+   <section id="portofolio" data-aos="flip-right"   data-aos-delay="50" data-aos-duration="2000"> 
+<button id="tombol2">Adobe Indesign</button>
+<div id="galeri2" class="sembunyi2">
+    <div class="galeri2">
+    </div>
+   </section>
+   
+   <div class="modal2" id="modal2"> 
+       <img src="" alt="" class="gambar-besar2">
+       <span class="tutup2">&times;</span>
+   </div>
+
+
+   
+  <section id="kontak" data-aos="flip-down"   data-aos-delay="50" data-aos-duration="2000">
+  <div class="infokontak" data-aos="fade-up" data-aos-duration="3000">
+        <h5>Kontak Me</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+      </div>
+ </div>
+  <div></div>
+  <form action="kontak.php" method="post">
+    <label>Nama:</label>
+    <input class="text" type="text" name="nama"><br><br>
+    <label>Email:</label>
+    <input class="email" type="email" name="email"><br><br>
+    <label>Pesan:</label>
+    <textarea class="pesan" name="pesan"></textarea><br><br>
+    <button>Kirim</button>
+  </form>
+</section>
+
+<button id="kembali-atas" class="tombol_kembali_atas">
+<p>Kembali ke Atas</p>
+</button>
+
+</section>
+<input type="button" onClick="document.getElementById('middle').scrollIntoView();" />
+    <footer>
+    <p>&copy; Fickry A.R Copyright 2024 </p>
+    </footer>
+ 
+    <link rel="stylesheet" href="https://unpkg.com/aos@2.3.4/dist/aos.css" />
+<script src="https://unpkg.com/aos@2.3.4/dist/aos.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.0/gsap.min.js"></script>
+
+<script>
+  AOS.init({ once: true });
+
+  gsap.from("header", {
+    ease: "expo.out",
+    duration: 3,
+    y: "-100%",
+  });
+</script>
+
+    <script src="srcol.js"></script>
+ <script src="1.js"></script>
+ <script src="script2.js"></script>
+ <script src="menu.js"></script>
+</body>
+</html>
+
+
+
